906476d96a930b8bc1a5efd6f3381766c8d9dfd2
Anoxic <guanrunjie@gmail.com>
2015-11-29 20:42:27 -0800

    Move items to new location

---
+++ resource/new-site/main.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Anoxic on 112115.
+ */
+
+/**
+ * Enables the basic functions of navbar (animation)
+ * @author Anoxic
+ */
+function enableNavBar() {
+    $(".nav").hover(function() {
+        // Mousein
+        $(this).removeClass("hidden");
+    }, function() {
+        // Mouseout
+        $(this).addClass("hidden");
+    });
+}
+
+/**
+ * Enables the slider and the animation
+ * @author Anoxic
+ */
+function enableSlider() {
+    initSlider();
+    // Click to expand
+    $(".content-slider .toggle").click(function() {
+        var $parent = $(this).parent();
+
+        if ($parent.hasClass("expand")) {
+            // Go to leftmost
+            $parent.animate({
+                scrollLeft: 0
+            });
+        } else {
+            // Go to expand
+            $parent.animate({
+                scrollLeft: window.innerWidth - $(this).outerWidth()
+            });
+        }
+    });
+}
+
+/**
+ * Initiates the slider on the webpage
+ * @author Anoxic
+ */
+function initSlider() {
+
+}
+
+$(document).ready(function() {
+    enableNavBar();
+})
